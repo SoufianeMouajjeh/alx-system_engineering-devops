1f3e4858a9ddd262aeb0d764939dfbcac214303e
SoufianeMouajjeh <melmouajjeh@gmail.com>
2024-05-07 06:54:43 -0700

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,14 @@
+#!/usr/bin/python3
+""" GET how many subscribers a subreddit has """
+
 import requests
-""" Get how many subscribers """
+
+
 def number_of_subscribers(subreddit):
-    # Reddit API URL to get subreddit information
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-
-    # Custom User-Agent header to avoid Too Many Requests error
-    headers = {'User-Agent': 'Custom User Agent'}
-
-    # Send GET request to Reddit API
+    """ GET how many subscribers a subreddit has """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Python3'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-
-    # Check if the subreddit exists and the request was successful
-    if response.status_code == 200:
-        # Extract the number of subscribers from the response JSON
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
-        # Invalid subreddit or other error occurred
+    if response.status_code != 200:
         return 0
+    return response.json().get('data').get('subscribers')
